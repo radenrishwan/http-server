0fc14a6eaeefbd5fdc483bec777a5502b3e62916
radenrishwan <radenrishwan@gmail.com>
2023-12-17 09:40:22 +0700

    implement request (request struct)

--- src/request.rs
+++ src/request.rs
@@ -3,7 +3,7 @@ pub struct Request {
     pub method: String,
     pub path: String,
     pub version: String,
-    pub headers: Vec<(String, String)>,
+    pub headers: Vec<String>,
     pub body: String
 }
 
@@ -13,14 +13,22 @@ impl Request {
 
         let method = split[0];
         let path = split[1];
-        let version = split[2].split("\r\n").collect::<Vec<&str>>()[0]; 
+        let version = split[2].split("\r\n").collect::<Vec<&str>>()[0];
+
+        let headers: Vec<String> = request.lines()
+            .filter(|x| x.contains(":"))
+            .map(|x| x.to_string())
+            .collect();
+
+        let body = request.split("\r\n\r\n").collect::<Vec<&str>>()[1];
+        println!("body: {}", body);
 
         Request {
             method: method.to_string(),
             path: path.to_string(),
             version: version.to_string(),
-            headers: Vec::new(),
-            body: String::new()
+            headers: headers,
+            body: String::from(body),
         }
     }
 }
