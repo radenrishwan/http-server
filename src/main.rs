3148c83184242de07d4c1a0792669ae10eeaf7c7
Raden Mohamad Rishwan <103234055+radenrishwan@users.noreply.github.com>
2024-05-06 03:53:31 +0000

    implement get & set cookie

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,14 @@ mod request;
 mod response;
 mod utils;
 
+use chrono::{Utc, Duration};
+use tokio::io::AsyncWriteExt;
 use tokio::{io::AsyncReadExt, net::TcpListener};
 
 use crate::handler::file_handler;
 use crate::request::Request;
 use crate::response::Response;
+use crate::utils::cookie;
 
 #[tokio::main]
 async fn main() {
@@ -48,10 +51,34 @@ async fn main() {
                 response.send(stream).await.unwrap();
             }
 
+            if request.path == "/cookie" {
+                let current = Utc::now() + Duration::days(7);
+                let c = cookie::set("foo", "bar", "/cookie", ".localhost", current);
+
+               let cookies = cookie::get(request.headers); 
+                let mut body = "".to_string();
+
+                for (key, value) in cookies {
+                    body.push_str(format!("<h3>{}: {}<h3>", key, value).as_str());
+                }
+
+                let response = Response::new(
+                    "200 OK".to_string(),
+                    vec![
+                        c
+                    ],
+                    body,
+                );                
+
+                response.send(stream).await.unwrap();
+            }
+
             if request.path == "/serve" {
                 file_handler(stream, "static/somefile.html", "text/html").await.unwrap();
-                
             }
+
+            stream.shutdown().await.unwrap();
+            println!("Connection closed");
         });
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,30 +1,71 @@
-#[macro_use]
 pub mod cookie {
-    use std::io::{Error, ErrorKind};
-    use crate::response::Response;
+    use chrono::{DateTime, Utc};
 
-    pub fn set(key: &str, value: &str, path : &str, domain : &str, exp : &str) -> String {
-        format!("Set-Cookio: {}={}; expires={}, path={}, domain={}", key, value, exp, path, domain)
-   }
+    use std::{
+        collections::HashMap,
+        io::{Error, ErrorKind},
+    };
 
-    pub fn get(response: Response, key: &str) -> Result<&str, Error> {
-        return match response.headers.get("Cookie") || response.headers.get("cookie") {
-            Ok(cookies) => {
-                let cookies = cookies.split(";").collect::<Vec<&str>>();
-                for cookie in cookies {
-                    let cookie = cookie.split("=").collect::<Vec<&str>>();
-                    if cookie[0] == key {
-                        return Ok(cookie[1]);
-                    }
+    /// set cookie
+    ///
+    /// examples:
+    /// ```
+    /// let current = Utc::now() + Duration::days(7);
+    ///
+    /// let c = cookie::set("foo", "bar", "/cookie", ".localhost", formatted_time.as_str());
+    /// let response = Response::new(
+    ///     "200 OK".to_string(),
+    ///     vec![
+    ///         c
+    ///     ],
+    ///     body,
+    /// );
+    ///
+    /// response.send(stream).await.unwrap();
+    /// ```
+    pub fn set(key: &str, value: &str, path: &str, domain: &str, exp: DateTime<Utc>) -> String {
+        let formatted_time = exp.format("%a, %e %b %Y %H:%M:%S GMT").to_string();
+        if domain == "" {
+            return format!(
+                "Set-Cookie: {}={}; path={}; expires={}; Secure; SameSite=None",
+                key, value, path, formatted_time
+            );
+        }
+
+        format!(
+            "Set-Cookie: {}={}; path={}; expires={}; domain={}; Secure; SameSite=None",
+            key, value, path, formatted_time, domain
+        )
+    }
+
+    pub fn get(headers: Vec<String>) -> HashMap<String, String> {
+        let mut result = HashMap::new();
+        // split cookies
+        for header in headers {
+            if header.contains("Cookie:") {
+                let cookie = header.split("Cookie:").collect::<Vec<&str>>()[1];
+                let cookie = cookie.split(";").collect::<Vec<&str>>();
+
+                for c in cookie {
+                    let cookie = c.split("=").collect::<Vec<&str>>();
+                    result.insert(cookie[0].to_string(), cookie[1].to_string());
                 }
 
-                Ok("")
-            },
-            Err(_) => {
-                Err(Error::new(ErrorKind::NotFound, "Cookie not found"))
-            },
-        };
+                break;
+            }
+        }
 
+        result
+    }
 
+    pub fn get_value(headers: Vec<String>, key: &str) -> Result<String, Error> {
+        // get cookie
+        let cookies = get(headers);
+
+        if cookies.contains_key(key) {
+            return Ok(cookies.get(key).unwrap().to_string());
+        }
+
+        Err(Error::new(ErrorKind::NotFound, "Cookie not found"))
     }
 }
